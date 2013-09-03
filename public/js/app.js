0aac95f0217a73864118eb9e5bea9db06dc41e80
George Diaz <george.diaz@northwestern.edu>
2013-09-03 16:51:46 -0500

    Add Angular/Bootstrap

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+// app.js
+var app = angular.module("halon",[ ]);
+
+// app.config(['$routeProvider',function($routeProvider){
+//         $routeProvider.when('/',{templateUrl:'app/partials/login.html', controller: 'loginController'})
+//         $routeProvider.otherwise({redirectTo:'/'})
+// }])
+
+// controller.js
+app.controller('loginController',function($scope){
+    //controller code will be inserted here
+})
