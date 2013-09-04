6aa62517235a4e40126bb67b2de26d82da088966
George Diaz <georgediaz88@yahoo.com>
2013-09-04 13:18:12 -0500

    Add Angular Route for Printers

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,7 +1,1 @@
 @extends("layouts.master")
-
-@section('content')
-  <h2>This is an Angular Box in index.blade.php</h2>
-  <input type="text" placeholder="I use Angular!" ng-model="bindExampleOne">
-  {{bindExampleOne}}
-@stop
--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,15 @@ var app = angular.module("halon",[ ]);
 
 app.config(['$routeProvider',function($routeProvider){
         $routeProvider.when('/',{templateUrl:'app/partials/login.html', controller: 'loginController'})
+        $routeProvider.when('/printers',{templateUrl:'app/partials/printers.html', controller: 'printerController'})
         $routeProvider.otherwise({redirectTo:'/'})
 }])
 
 // controller.js
 app.controller('loginController',function($scope){
     //controller code will be inserted here
+})
+
+app.controller('printerController',function($scope){
+    // code
 })
