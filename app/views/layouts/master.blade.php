60e5f5a0adfde3cec879aa7b79b60d1517888678
George Diaz <georgediaz88@yahoo.com>
2013-09-04 16:25:53 -0500

    Bootstrap NavBar with ActiveTabs

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Halon</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- Add Bootstrap 3.0 -->
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/bootstrap-theme.min.css" rel="stylesheet">
@@ -12,7 +13,32 @@
 
   <body ng-app="halon">
     @section('sidebar')
-      This is the master sidebar.
+    <nav class="navbar navbar-inverse" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Halon</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav">
+          <li ng-class="{active: location.path() == '/'}"><a href="#">Home</a></li>
+          <li ng-class="{active: location.path() == '/printers'}"><a href="#/printers">Printers</a></li>
+        </ul>
+        <form class="navbar-form navbar-right" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+      </div>
+    </nav>
     @show
 
     <div class="container">
@@ -25,6 +51,7 @@
     <script src="angular/angular-resource.min.js"></script>
     <script src="angular/angular-sanitize.min.js"></script>
     <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/app.js"></script>
 
 <!-- Not Yet Implemented but for better seperation later
--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,16 @@
 var app = angular.module("halon",[ ]);
 
 app.config(['$routeProvider',function($routeProvider){
-        $routeProvider.when('/',{templateUrl:'app/partials/login.html', controller: 'loginController'})
-        $routeProvider.when('/printers',{templateUrl:'app/partials/printers.html', controller: 'printerController'})
-        $routeProvider.otherwise({redirectTo:'/'})
+  $routeProvider.when('/',{templateUrl:'app/partials/login.html', controller: 'loginController'})
+  $routeProvider.when('/printers',{templateUrl:'app/partials/printers.html', controller: 'printerController'})
+  $routeProvider.otherwise({redirectTo:'/'})
 }])
 
 // controller.js
-app.controller('loginController',function($scope){
-    //controller code will be inserted here
+app.controller('loginController',function($scope, $rootScope, $location){
+  $rootScope.location = $location; // used for ActiveTab
 })
 
-app.controller('printerController',function($scope){
-    // code
+app.controller('printerController',function($scope, $rootScope, $location){
+  $rootScope.location = $location; // used for ActiveTab
 })
