3a0106e0ea573ea30a8503f9ece47deb13380de5
George Diaz <georgediaz88@yahoo.com>
2013-09-06 17:09:09 -0500

    Add Admin Pswd Regenerate Logic/Page Fully loaded using AngularJS for ClientSide Password Algorithm found provided and passing in a param of integer will set to that length

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ var app = angular.module("halon",['ui.bootstrap','ngResource','ngSanitize']);
 app.config(['$routeProvider',function($routeProvider){
   $routeProvider.when('/',{templateUrl:'app/partials/login.html', controller: 'loginController'});
   $routeProvider.when('/locations',{templateUrl:'app/partials/locations.html', controller: 'locationController'});
+  $routeProvider.when('/admin/manage',{templateUrl:'app/partials/users.html', controller: 'userController'});
   $routeProvider.otherwise({redirectTo:'/'});
 }]);
 
@@ -45,6 +46,27 @@ app.controller('locationController',function($scope, $rootScope, $location, Auth
   })
 })
 
+app.controller('userController',function($scope, $rootScope, $sanitize, $location, $resource, Authenticate, User, PasswordService){
+  $rootScope.location = $location; // used for ActiveTab
+  $scope.users = User.query();
+
+ $scope.regenerate = function() {
+  var random_pass = PasswordService.generate(12);
+  var usr = $scope.users[this.$index];
+  usr.password = $sanitize(random_pass);
+  usr.$update({}, function() {
+    alert("Password Reset to: " + random_pass);
+  })
+  // var all_users = User.query({userId:id_field}, function() {
+  //   var user1 = $scope.users[0];
+  //   user1.password = $sanitize(random_pass);
+  //   user1.$update({}, function() {
+  //     alert("Password Reset to: " + random_pass);
+  //   })
+  // });
+ }
+})
+
 //factories.js
 app.factory('Authenticate', function($resource){
     return $resource("/service/authenticate");
@@ -52,6 +74,10 @@ app.factory('Authenticate', function($resource){
 
 app.factory('Location', function($resource){
     return $resource("/locations");
+})
+
+app.factory('User', function($resource){
+    return $resource('/users/:userId', {userId:'@id'}, {update: { method: 'PUT' }});
 })
 
 app.factory('FlashService', ['$rootScope', function($rootScope) {
@@ -70,4 +96,34 @@ app.factory('FlashService', ['$rootScope', function($rootScope) {
   }
   return alertService;
 }]);
+
+app.factory('PasswordService', function () {
+  // ref: http://stackoverflow.com/questions/1497481/javascript-password-generator
+  var features = {
+    generate: function(plength){
+      var keylistalpha_low ="abcdefghijklmnopqrstuvwxyz";
+      var keylistalpha = keylistalpha_low + keylistalpha_low.toUpperCase();
+      var keylistint="123456789";
+      var keylistspec="!@#_";
+      var temp='';
+      var len = plength/2;
+      var len = len - 1;
+      var lenspec = plength-len-len;
+
+      for (i=0;i<len;i++)
+          temp+=keylistalpha.charAt(Math.floor(Math.random()*keylistalpha.length));
+
+      for (i=0;i<lenspec;i++)
+          temp+=keylistspec.charAt(Math.floor(Math.random()*keylistspec.length));
+
+      for (i=0;i<len;i++)
+          temp+=keylistint.charAt(Math.floor(Math.random()*keylistint.length));
+
+          temp=temp.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+      return temp;
+    }
+  }
+  return features;
+})
 
