db40fbef8f154a2360f57d489b491ae61fdbcfd1
John Dzak <j-dzak@northwestern.edu>
2013-09-12 21:14:47 -0500

    Add view show/hide of navigation links and role based login redirection. #6

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -28,8 +28,8 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav">
-          <li ng-class="{active: location.path() == '/locations'}"><a href="#/locations">Locations</a></li>
-          <li ng-class="{active: location.path() == '/admin/manage'}"><a href="#/admin/manage">User Passwords</a></li>
+          <li ng-show="can('update', 'Location')" ng-class="{active: location.path() == '/locations'}"><a href="#/locations">Locations</a></li>
+          <li ng-show="can('update', 'Location')" ng-class="{active: location.path() == '/admin/manage'}"><a href="#/admin/manage">User Passwords</a></li>
         </ul>
         <div class="navbar-form navbar-right" ng-show="authenticated()" role="search">
           <button class="btn btn-primary" ng-click="logout()">Sign Out</button>
