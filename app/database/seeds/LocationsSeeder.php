2946c7e75849ff9ce3159d479eb75ba40acb71c5
John Dzak <j-dzak@northwestern.edu>
2013-09-25 18:59:22 -0500

    Add nubic printer to location seeds. #4

--- app/database/seeds/LocationsSeeder.php
+++ app/database/seeds/LocationsSeeder.php
@@ -17,6 +17,13 @@ class LocationsSeeder extends Seeder {
       'printer_name'  => 'DeskJet 570',
       'mar_file_name' => '12_mar.ps'
     ));
+
+    Location::create(array(
+      'description'  => 'NUBIC',
+      'phone_number' => '123-321-1230',
+      'printer_name' => 'NPI6C6DB9 (HP LaserJet 500 colorMFP M570dn)',
+      'mar_file_name'=> 'bell_206.ps'
+    ));
   }
 }
 
