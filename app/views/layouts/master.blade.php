4906bef191b3282d1d056f3b6f85091d712dd607
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-10-09 13:19:31 -0500

    Add admin link to the navigation tab. Closes #17.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -32,6 +32,7 @@
           <li ng-show="permit('admin')" ng-class="{active: location.path() == '/admin/locations'}"><a href="#/admin/locations">Locations</a></li>
           <li ng-show="permit('admin')" ng-class="{active: location.path() == '/admin/manage'}"><a href="#/admin/manage">User Passwords</a></li>
           <li ng-show="permit('printer')" ng-class="{active: location.path() == '/locations'}"><a href="#/locations">Print MARs</a></li>
+          <li ng-show="!permit('admin')" ng-class="{active: location.path() == '/admin/locations'}"><a href="#/admin/locations">Admin</a></li>
         </ul>
         <div class="navbar-form navbar-right" ng-show="authenticated()" role="search">
           <button class="btn btn-primary" ng-click="logout()">Sign Out</button>
