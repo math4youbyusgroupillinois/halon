9e40be5f09c03d81ea097db19dfbbd806a5fde2d
John Dzak <j-dzak@northwestern.edu>
2013-11-07 13:38:07 -0600

    Add mar flag to print_jobs. #22

---
+++ app/database/migrations/2013_11_07_132941_add_mar_flag_to_print_jobs.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddMarFlagToPrintJobs extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::table('print_jobs', function($table) {
+      $table->boolean('mar');
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::table('print_jobs', function($table) {
+      $table->dropColumn('mar');
+    });
+	}
+
+}
